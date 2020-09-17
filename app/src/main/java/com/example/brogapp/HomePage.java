f97df5f86253f7803c5f9af829a38daafde1ca2d
kajgaard <maria.kajgaard@gmail.com>
2020-09-17 16:27:33 +0200

    Added navbar functionality for both homepage and profile page

--- app/src/main/java/com/example/brogapp/HomePage.java
+++ app/src/main/java/com/example/brogapp/HomePage.java
@@ -1,8 +1,13 @@
 package com.example.brogapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomePage extends AppCompatActivity {
 
@@ -10,5 +15,47 @@ public class HomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        //Initialize and assign navbar variable
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigationbar);
+
+        //Set home iteam as selected
+        bottomNavigationView.setSelectedItemId(R.id.nav_home);
+
+        //Set up listener, for determine if other icon is pressed
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+
+                    case R.id.nav_home:
+                        startActivity(new Intent(getApplicationContext(),HomePage.class));
+                        overridePendingTransition(0,0); //Dont know what this does
+                        return true;
+
+                    case R.id.nav_scan:
+                        //startActivity(new Intent(getApplicationContext(),ScanActivity.class));
+                        overridePendingTransition(0,0); //Dont know what this does
+                        return true;
+
+                    case R.id.nav_brew:
+                        startActivity(new Intent(getApplicationContext(),BrewMainActivity.class));
+                        overridePendingTransition(0,0); //Dont know what this does
+                        return true;
+
+                    case R.id.nav_wash:
+                        //startActivity(new Intent(getApplicationContext(),CleanActivity.class));
+                        overridePendingTransition(0,0); //Dont know what this does
+                        return true;
+
+                    case R.id.nav_profile:
+                        startActivity(new Intent(getApplicationContext(),ProfilePage.class));
+                        overridePendingTransition(0,0); //Dont know what this does
+                        return true;
+                }
+
+                return false;
+            }
+        });
     }
 }
