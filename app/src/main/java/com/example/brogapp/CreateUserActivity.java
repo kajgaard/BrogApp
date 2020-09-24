d1673394222a312de2d6ab8f1a0aa4735072be17
kajgaard <maria.kajgaard@gmail.com>
2020-09-24 12:54:58 +0200

    Changed id names to correct

--- app/src/main/java/com/example/brogapp/CreateUserActivity.java
+++ app/src/main/java/com/example/brogapp/CreateUserActivity.java
@@ -3,12 +3,28 @@ package com.example.brogapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class CreateUserActivity extends AppCompatActivity {
+
+    TextView logIn;
+    EditText name, email, password;
+    Button save;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_user);
+
+        name = findViewById(R.id.nameET);
+        email = findViewById(R.id.emailET);
+        password = findViewById(R.id.newPasswordET);
+
+        logIn = findViewById(R.id.alreadyUserTV);
+        save = findViewById(R.id.createUserBtn);
+
+
     }
 }
