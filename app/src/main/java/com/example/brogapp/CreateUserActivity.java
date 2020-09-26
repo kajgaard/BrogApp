53c9adb1a9d5c8f8eed3713a312ab22d5b8f9017
kajgaard <maria.kajgaard@gmail.com>
2020-09-26 13:24:16 +0200

    Users are now created in collection in database beside authentication

--- app/src/main/java/com/example/brogapp/CreateUserActivity.java
+++ app/src/main/java/com/example/brogapp/CreateUserActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,17 +15,27 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CreateUserActivity extends AppCompatActivity implements View.OnClickListener {
 
+    public static final String TAG = "TAG";
     TextView alreadyUser;
     EditText nameET, emailET, passwordET;
     Button save;
     ProgressBar progressBar;
     FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +45,7 @@ public class CreateUserActivity extends AppCompatActivity implements View.OnClic
 
 
         fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
         nameET = findViewById(R.id.nameET);
         nameET.requestFocus();
         emailET = findViewById(R.id.newEmailET);
@@ -57,8 +69,8 @@ public class CreateUserActivity extends AppCompatActivity implements View.OnClic
             startActivity(new Intent(getApplicationContext(),LogInActivity.class));
             finish();
         }else if(view == save){
-            String name = nameET.getText().toString().trim();
-            String email = emailET.getText().toString().trim();
+            final String name = nameET.getText().toString().trim();
+            final String email = emailET.getText().toString().trim();
             String password = passwordET.getText().toString().trim();
 
             //These loops checks if input is valid
@@ -86,6 +98,23 @@ public class CreateUserActivity extends AppCompatActivity implements View.OnClic
                 public void onComplete(@NonNull Task<AuthResult> task) {
                     if(task.isSuccessful()){
                         Toast.makeText(CreateUserActivity.this, "Bruger oprettet", Toast.LENGTH_SHORT).show();
+                        userID = fAuth.getCurrentUser().getUid();
+                        DocumentReference documentReference = fStore.collection("users").document(userID);
+                        Map<String,Object> user = new HashMap<>();
+                        user.put("name",name);
+                        user.put("email",email);
+                        user.put("userID",userID);
+                        documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Log.d(TAG, "Succes; User created for "+userID);
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.d(TAG, "onFailure" + e.toString());
+                            }
+                        });
                         startActivity(new Intent(getApplicationContext(),HomePage.class));
                     }else{
                         Toast.makeText(CreateUserActivity.this, "Fejl " + task.getException(), Toast.LENGTH_SHORT).show();
@@ -93,6 +122,5 @@ public class CreateUserActivity extends AppCompatActivity implements View.OnClic
                 }
             });
         }
-
     }
 }
