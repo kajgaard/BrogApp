19bfd6619752fb889278f2cde17edc236172a283
kajgaard <maria.kajgaard@gmail.com>
2020-10-01 16:16:13 +0200

    Manual merge from master +  populating database in clean activity

--- app/src/main/java/com/example/brogapp/CleanActivity.java
+++ app/src/main/java/com/example/brogapp/CleanActivity.java
@@ -6,15 +6,97 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 
-public class CleanActivity extends AppCompatActivity {
+import java.util.HashMap;
+import java.util.Map;
+
+public class CleanActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView tv;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clean);
+
+        tv = findViewById(R.id.textView);
+        tv.setOnClickListener(this);
+
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+        userID = fAuth.getCurrentUser().getUid();
+
+        //For populating database
+        Map<String, Object> newbrew = new HashMap<>();
+        newbrew.put("brewName", "Sunday");
+        newbrew.put("brewDescription", "Smager meget godt");
+        newbrew.put("brewScore", "4.1");
+        newbrew.put("imageRessource", "2131165293");
+        newbrew.put("coffeeAmount", "30");
+        newbrew.put("grindSize", "medium");
+        newbrew.put("waterRatio", "70");
+        newbrew.put("brewTemp", "93");
+        newbrew.put("bloomWater", "45");
+        newbrew.put("bloomTime", "30");
+        newbrew.put("brewTime", "180");
+
+        Map<String, Object> newbrew1 = new HashMap<>();
+        newbrew1.put("brewName", "London");
+        newbrew1.put("brewDescription", "God med Starbuks bønnerne!");
+        newbrew1.put("brewScore", "4.5");
+        newbrew1.put("imageRessource", "2131165293");
+        newbrew1.put("coffeeAmount", "20");
+        newbrew1.put("grindSize", "medium");
+        newbrew1.put("waterRatio", "60");
+        newbrew1.put("brewTemp", "93");
+        newbrew1.put("bloomWater", "45");
+        newbrew1.put("bloomTime", "30");
+        newbrew1.put("brewTime", "180");
+
+
+        Map<String, Object> newbrew2 = new HashMap<>();
+        newbrew2.put("brewName", "New York");
+        newbrew2.put("brewDescription", "Når veninderne er på besøg!");
+        newbrew2.put("brewScore", "3.8");
+        newbrew2.put("imageRessource", "2131165293");
+        newbrew2.put("coffeeAmount", "20");
+        newbrew2.put("grindSize", "medium");
+        newbrew2.put("waterRatio", "60");
+        newbrew2.put("brewTemp", "93");
+        newbrew2.put("bloomWater", "45");
+        newbrew2.put("bloomTime", "30");
+        newbrew2.put("brewTime", "180");
+
+        Map<String, Object> newbrew3 = new HashMap<>();
+        newbrew3.put("brewName", "Copenhagen");
+        newbrew3.put("brewDescription", "Svigermors ynglings! Peter kan ikke lide den så meget");
+        newbrew3.put("brewScore", "4.3");
+        newbrew3.put("imageRessource", "2131165293");
+        newbrew3.put("coffeeAmount", "20");
+        newbrew3.put("grindSize", "medium");
+        newbrew3.put("waterRatio", "60");
+        newbrew3.put("brewTemp", "93");
+        newbrew3.put("bloomWater", "45");
+        newbrew3.put("bloomTime", "30");
+        newbrew3.put("brewTime", "180");
+
+
+
+        fStore.collection("users").document(userID).collection("favorites").document().set(newbrew);
+        fStore.collection("users").document(userID).collection("favorites").document().set(newbrew1);
+        fStore.collection("users").document(userID).collection("favorites").document().set(newbrew2);
+        fStore.collection("users").document(userID).collection("favorites").document().set(newbrew3);
 
         //Initialize and assign navbar variable
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigationbar);
@@ -56,5 +138,12 @@ public class CleanActivity extends AppCompatActivity {
                 return false;
             }
         });
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == tv){
+
+        }
     }
 }
