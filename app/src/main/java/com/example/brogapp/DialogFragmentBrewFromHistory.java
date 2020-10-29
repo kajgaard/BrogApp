d2873a0a42d4724edcd12c408bdb635b513dce7f
Maria <maria.kajgaard@gmail.com>
2020-10-29 11:29:32 +0100

    created DialogFragmentBrewFromHistory

---
+++ app/src/main/java/com/example/brogapp/DialogFragmentBrewFromHistory.java
@@ -0,0 +1,20 @@
+package com.example.brogapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+public class DialogFragmentBrewFromHistory extends DialogFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.tap_brew_in_history_fragtment,container,false);
+    }
+}
